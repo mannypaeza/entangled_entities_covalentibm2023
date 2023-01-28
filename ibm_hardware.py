194e202d72299748a959f460feea07dd2e15c3e1
Manuel Paez <mannypaezjr@comcast.net>
2023-01-28 16:27:26 -0500

    made changes

--- ibm_hardware.py
+++ ibm_hardware.py
@@ -1,9 +1,28 @@
 import qiskit
+from qiskit import *
 from qiskit import IBMQ
+from qiskit.compiler import assemble, transpile
+from qiskit.providers.fake_provider import FakeNairobiV2
 
-#get account
-IBMQ.enable_account(TOKEN, hub='ibm-q-community', group='mit-hackathon', project='main')
+""" 
+Get account/backend (REAL)
+"""
+#Token = User's ID
+#IBMQ.enable_account(TOKEN, hub='ibm-q-community', group='mit-hackathon', project='main')
+#provider = IBMQ.get_provider(hub='ibm-q-community', group='mit-hackathon', project='main')
+#backend = provider.get_backend('ibm-q-community')
 
-#get provider
-provider = IBMQ.get_provider(hub='ibm-q-community', group='mit-hackathon', project='main')
-provider.backends()
+"""
+Get account/backend (Fake)
+"""
+
+backend = FakeNairobiV2
+
+"""
+Retrieving Job
+"""
+
+#Retriving the Job
+job = backend.run()
+result = job.result()
+
