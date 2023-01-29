1384305167a1a4c9e7460c3bcd9d1081e30aa8d6
Manuel Paez <mannypaezjr@comcast.net>
2023-01-28 19:25:20 -0500

    changes

--- graph.py
+++ graph.py
@@ -9,6 +9,15 @@ from qiskit_aqua import Operator, run_algorithm, get_algorithm_instance
 from qiskit_aqua.input import get_input_instance
 from qiskit_aqua.translators.ising import maxcut, tsp
 
+from qiskit import QuantumCircuit, 
+
 # nodes 
 n = 20
+
+#Graph Model with Edges (basic)
+G = nx.Graph()
+G.add_nodes_from([0, 1, 2, 3])
+G.add_edges_from([(0, 1), (1, 2)])
+nx.draw(G, with_labels=True, alpha=0.8, node_size=500)
+
 
