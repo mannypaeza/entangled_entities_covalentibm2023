9d3e50abc88c85c0d55d652ce354bfb6be08b838
Manuel Paez <mannypaezjr@comcast.net>
2023-01-29 01:25:21 -0500

    made some changes

--- graph.py
+++ graph.py
@@ -5,9 +5,6 @@ import qiskit
 from qiskit import *
 
 from qiskit.tools.visualization import plot_histogram
-from qiskit_aqua import Operator, run_algorithm, get_algorithm_instance
-from qiskit_aqua.input import get_input_instance
-from qiskit_aqua.translators.ising import maxcut, tsp
 
 from qiskit import QuantumCircuit, 
 
@@ -20,4 +17,6 @@ G.add_nodes_from([0, 1, 2, 3])
 G.add_edges_from([(0, 1), (1, 2)])
 nx.draw(G, with_labels=True, alpha=0.8, node_size=500)
 
+nqubits = 7
 
+qc = 
